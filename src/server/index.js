d8c15a008f8ee9df4b27f1c8ca0796a9de774447
discrete-projects <aterruso@gmail.com>
2018-12-10 15:21:52 -0500

    build

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,9 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/', (req, res) => {
+    
+});
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
